28102dc4b2224dc3dd8f2aa7b07ba3bffe726664
Pato Lankenau <rust@p.lankenau.io>
2022-05-31 17:19:00 +0200

    Mucking around
    Supposedly im writing text, but my attempt at rendering the pixel buffer
    using ppm isnt giving me useful output. I will try rendering the buffer
    on my actual eInk device before i waste more time debugging the ppm
    rendering.

--- src/lib.rs
+++ src/lib.rs
@@ -1,8 +1,84 @@
+use std::{
+    fs::File,
+    io::{self, BufWriter, Write},
+};
+
+use embedded_graphics::{
+    mono_font::MonoTextStyleBuilder,
+    prelude::*,
+    primitives::{Circle, Line, PrimitiveStyleBuilder},
+    text::{Baseline, Text, TextStyleBuilder},
+};
+use epd_waveshare::{
+    color::*,
+    epd4in2::{Display4in2, Epd4in2},
+    graphics::DisplayRotation,
+    prelude::*,
+};
+use ppm::write_buffer;
+
+mod ppm;
+
+/// Based on the GDEW042T2 e-Paper from Dalian Good Display Co., Ltd.: www.good-display.com
+#[derive(Default)]
+pub struct EinkDisplay {
+    disp: Display4in2,
+}
+
+impl EinkDisplay {
+    pub fn new() -> Self {
+        let mut disp = Display4in2::default();
+        disp.set_rotation(DisplayRotation::Rotate0);
+        disp.clear_buffer(Color::White);
+        Self { disp }
+    }
+
+    pub fn as_json_arr() -> String {
+        format!("todo")
+    }
+
+    pub fn as_ppm() -> String {
+        todo!()
+    }
+
+    pub fn save_to_file(&self, file_name: &str) -> io::Result<()> {
+        // create the output file
+        let file = File::create(file_name)?;
+        // let's avoid a syscall per write and buffer our writes
+        let buffer_size = 1 * 1024 * 1024; // 1 MB at a time
+        let mut writer = BufWriter::with_capacity(buffer_size, file);
+
+        write_buffer(141, 106, self.disp.buffer(), &mut writer)?;
+        writer.flush()?;
+
+        Ok(())
+    }
+
+    fn draw_text(&mut self, text: &str, x: i32, y: i32) {
+        let style = MonoTextStyleBuilder::new()
+            .font(&embedded_graphics::mono_font::ascii::FONT_10X20)
+            .text_color(White)
+            .background_color(Black)
+            .build();
+
+        let text_style = TextStyleBuilder::new().baseline(Baseline::Top).build();
+
+        let _ =
+            Text::with_text_style(text, Point::new(x, y), style, text_style).draw(&mut self.disp);
+    }
+}
+
 #[cfg(test)]
 mod tests {
+    use super::*;
+
     #[test]
     fn it_works() {
-        let result = 2 + 2;
-        assert_eq!(result, 4);
+        let mut display = EinkDisplay::default();
+        display.draw_text("Hello rust!", 175, 250);
+
+        display
+            .save_to_file("/tmp/eink.ppm")
+            .expect("failed to render");
     }
 }
---
+++ src/ppm.rs
@@ -0,0 +1,122 @@
+use std::io::{self, Write};
+
+use smallvec::SmallVec;
+
+const PPM_MAGIC: &[u8] = "P3".as_bytes();
+const PPM_NEW_LINE: &[u8] = "\n".as_bytes();
+const PPM_SPACE: &[u8] = " ".as_bytes();
+const PPM_MAX_COLOR_VAL: &[u8] = "255".as_bytes();
+const PPM_MAX_CHARS_PER_LINE: usize = 70;
+
+const ZERO_DIGIT_BYTE: u8 = 48;
+const SPACE_BYTE: u8 = 32;
+
+fn write_ppm_header(width: usize, height: usize, writer: &mut impl Write) -> io::Result<usize> {
+    let mut written = 0;
+
+    written += writer.write(PPM_MAGIC)?;
+    written += writer.write(PPM_NEW_LINE)?;
+    written += writer.write(&small_u32_as_utf8(width as u32))?;
+    written += writer.write(PPM_SPACE)?;
+    written += writer.write(&small_u32_as_utf8(height as u32))?;
+    written += writer.write(PPM_NEW_LINE)?;
+    written += writer.write(PPM_MAX_COLOR_VAL)?;
+    written += writer.write(PPM_NEW_LINE)?;
+
+    Ok(written)
+}
+
+pub fn write_buffer(
+    width: usize,
+    height: usize,
+    buf: &[u8],
+    writer: &mut impl Write,
+) -> io::Result<()> {
+    write_ppm_header(width, height, writer)?;
+
+    let mut written_in_line = 0;
+    for (idx, c) in buf.iter().enumerate() {
+        let encoded_c = pixel_as_utf8(*c);
+
+        // note: `+ 1` because we need to ensure we'd be able to fit the `\n`
+        let pixel_fits_in_line = written_in_line + encoded_c.len() + 1 > PPM_MAX_CHARS_PER_LINE;
+        let need_new_row = idx > 0 && idx % width == 0;
+        if pixel_fits_in_line || need_new_row {
+            writer.write_all(PPM_NEW_LINE)?;
+            written_in_line = 0;
+        } else if idx > 0 {
+            written_in_line += writer.write(PPM_SPACE)?;
+        }
+        written_in_line += writer.write(&encoded_c)?;
+    }
+
+    writer.write_all(PPM_NEW_LINE)?;
+    writer.write_all(PPM_NEW_LINE)?;
+
+    Ok(())
+}
+
+#[inline(always)]
+pub fn small_u32_as_utf8(val: u32) -> SmallVec<[u8; 8]> {
+    debug_assert!(val < 100000);
+
+    let mut vec = SmallVec::with_capacity(8);
+    let mut val = val;
+    for _ in 0..6 {
+        vec.insert(0, (val % 10) as u8 + ZERO_DIGIT_BYTE);
+        val /= 10;
+        if val == 0 {
+            break;
+        }
+    }
+
+    vec
+}
+
+#[inline(always)]
+pub fn pixel_as_utf8(pixel: u8) -> SmallVec<[u8; 16]> {
+    let r = pixel as u32;
+    let r = 255 - r; // flip it because the e-reader has 0xFF as black
+    let g = r;
+    let b = r;
+
+    let mut vec = SmallVec::with_capacity(16);
+
+    // encode b
+    let mut val = b;
+    for _ in 0..3 {
+        vec.insert(0, (val % 10) as u8 + ZERO_DIGIT_BYTE);
+        val /= 10;
+        if val == 0 {
+            break;
+        }
+    }
+
+    // space
+    vec.insert(0, SPACE_BYTE);
+
+    // encode g
+    let mut val = g;
+    for _ in 0..3 {
+        vec.insert(0, (val % 10) as u8 + ZERO_DIGIT_BYTE);
+        val /= 10;
+        if val == 0 {
+            break;
+        }
+    }
+
+    // space
+    vec.insert(0, SPACE_BYTE);
+
+    // encode r
+    let mut val = r;
+    for _ in 0..3 {
+        vec.insert(0, (val % 10) as u8 + ZERO_DIGIT_BYTE);
+        val /= 10;
+        if val == 0 {
+            break;
+        }
+    }
+
+    vec
+}
