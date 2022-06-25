39a92cee7165f4bfef72194711abe61a76608c59
Pato Lankenau <rust@p.lankenau.io>
2022-06-26 00:47:26 +0200

    draw the next race

--- examples/draw.rs
+++ examples/draw.rs
@@ -1,5 +1,8 @@
 use embedded_graphics::{
-    mono_font::{ascii::FONT_6X9, MonoTextStyle, MonoTextStyleBuilder},
+    mono_font::{
+        ascii::{FONT_10X20, FONT_6X9},
+        MonoTextStyle, MonoTextStyleBuilder,
+    },
     pixelcolor::BinaryColor,
     prelude::*,
     primitives::{Circle, Line, PrimitiveStyle, Rectangle},
@@ -14,36 +17,49 @@ fn main() -> Result<(), core::convert::Infallible> {
     let mut display = SimulatorDisplay::<BinaryColor>::new(Size::new(400, 300));
 
     let line_style = PrimitiveStyle::with_stroke(BinaryColor::On, 1);
-    let text_style = MonoTextStyle::new(&FONT_6X9, BinaryColor::On);
+    let small_text_style = MonoTextStyle::new(&FONT_6X9, BinaryColor::On);
+    let big_text_style = MonoTextStyle::new(&FONT_10X20, BinaryColor::On);
 
-    Circle::new(Point::new(72, 8), 48)
+    let gap = 60_u32;
+    let circle_diameter = 48_u32;
+    let square_length = 34_u32;
+    for i in 0..5_i32 {
+        Circle::new(Point::new(67 + i * (gap as i32), 8), circle_diameter)
+            .into_styled(line_style)
+            .draw(&mut display)?;
+
+        Rectangle::new(
+            Point::new(74 + i * (gap as i32), 15),
+            Size::new(square_length, square_length),
+        )
         .into_styled(line_style)
         .draw(&mut display)?;
+    }
 
-    Line::new(Point::new(48, 16), Point::new(8, 16))
-        .into_styled(line_style)
-        .draw(&mut display)?;
+    // Text::new("Hello World!", Point::new(5, 12), text_style).draw(&mut display)?;
 
-    Line::new(Point::new(48, 16), Point::new(64, 32))
-        .into_styled(line_style)
-        .draw(&mut display)?;
+    // let style = MonoTextStyleBuilder::new()
+    //     .font(&embedded_graphics::mono_font::ascii::FONT_10X20)
+    //     .text_color(White)
+    //     .background_color(Black)
+    //     .build();
 
-    Rectangle::new(Point::new(79, 15), Size::new(34, 34))
-        .into_styled(line_style)
-        .draw(&mut display)?;
+    // let text_style = TextStyleBuilder::new().baseline(Baseline::Top).build();
 
-    Text::new("Hello World!", Point::new(5, 12), text_style).draw(&mut display)?;
+    let _ = Text::new("Next race:", Point::new(150, 100), big_text_style).draw(&mut display)?;
 
-    let style = MonoTextStyleBuilder::new()
-        .font(&embedded_graphics::mono_font::ascii::FONT_10X20)
-        .text_color(White)
-        .background_color(Black)
-        .build();
+    let _ =
+        Text::new("British Grand Prix", Point::new(100, 125), big_text_style).draw(&mut display)?;
+    let _ = Text::new(
+        "Silverstone Circuit",
+        Point::new(140, 150),
+        small_text_style,
+    )
+    .draw(&mut display)?;
+    let _ = Text::new("England", Point::new(170, 161), small_text_style).draw(&mut display)?;
 
-    let text_style = TextStyleBuilder::new().baseline(Baseline::Top).build();
-
-    let _ = Text::with_text_style("Hello rust!", Point::new(175, 250), style, text_style)
-        .draw(&mut display);
+    // let _ = Text::with_text_style("Hello rust!", Point::new(175, 250), style, text_style)
+    //     .draw(&mut display);
 
     let output_settings = OutputSettingsBuilder::new()
         .theme(BinaryColorTheme::OledWhite)
